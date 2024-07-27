7ea3e436d501ad220707861e317e03e4659fc067
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-07-27 18:13:29 +0530

    terraform with jenkins

--- terraform-aws-tools/cicd/data.tf
+++ terraform-aws-tools/cicd/data.tf
@@ -1,41 +1,41 @@
-data "aws_ami" "ami_info" {
+# data "aws_ami" "ami_info" {
 
-    most_recent = true
-    owners = ["851725509871"]
+#     most_recent = true
+#     owners = ["851725509871"]
 
-    filter {
-        name   = "name"
-        values = ["RHEL-9-DevOps-Practice"]
-    }
+#     filter {
+#         name   = "name"
+#         values = ["RHEL-9-DevOps-Practice"]
+#     }
 
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+#     filter {
+#         name   = "root-device-type"
+#         values = ["ebs"]
+#     }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
+#     filter {
+#         name   = "virtualization-type"
+#         values = ["hvm"]
+#     }
+# }
 
-data "aws_ami" "nexus_ami_info" {
+# data "aws_ami" "nexus_ami_info" {
 
-    most_recent = true
-    owners = ["851725509871"]
+#     most_recent = true
+#     owners = ["851725509871"]
 
-    filter {
-        name   = "name"
-        values = ["SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-*"]
-    }
+#     filter {
+#         name   = "name"
+#         values = ["SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-*"]
+#     }
 
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+#     filter {
+#         name   = "root-device-type"
+#         values = ["ebs"]
+#     }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
+#     filter {
+#         name   = "virtualization-type"
+#         values = ["hvm"]
+#     }
+# }
--- terraform-aws-tools/cicd/main.tf
+++ terraform-aws-tools/cicd/main.tf
@@ -1,33 +1,70 @@
-module "jenkins" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+# resource <resource-type> <resource-name>
+resource "aws_instance" "jenkins" {
 
-  name = "jenkins-tf"
+    ami = "ami-041e2ea9402c46c32"  
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = "t3.micro"
 
-  instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0023be3b9dcdb312d"] #replace your SG
-  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
-  ami = data.aws_ami.ami_info.id
-  user_data = file("jenkins.sh")
-  tags = {
-    Name = "jenkins-tf"
-  }
+    tags = {
+        Name = "jenkins"
+    }
 }
 
-module "jenkins_agent" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allowing SSH access"
 
-  name = "jenkins-agent"
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-  instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0023be3b9dcdb312d"]
-  # convert StringList to list and get first element
-  subnet_id = "subnet-0ea509ad4cba242d7"
-  ami = data.aws_ami.ami_info.id
-  user_data = file("jenkins-agent.sh")
-  tags = {
-    Name = "jenkins-agent"
-  }
+    egress {
+        from_port        = 0 # from 0 to 0 means, opening all protocols
+        to_port          = 0
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "nirmala"
+    }
 }
+
+
+# module "jenkins" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "jenkins-tf"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-0023be3b9dcdb312d"] #replace your SG
+#   subnet_id = [10.0.0.0/] #replace your Subnet
+#   ami = data.aws_ami.ami_info.id
+#   user_data = file("jenkins.sh")
+#   tags = {
+#     Name = "jenkins-tf"
+#   }
+# }
+
+# module "jenkins_agent" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "jenkins-agent"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-0023be3b9dcdb312d"]
+#   # convert StringList to list and get first element
+#   subnet_id = "subnet-0ea509ad4cba242d7"
+#   ami = data.aws_ami.ami_info.id
+#   user_data = file("jenkins-agent.sh")
+#   tags = {
+#     Name = "jenkins-agent"
+#   }
+# }
 
 resource "aws_key_pair" "tools" {
   key_name   = "tools"
@@ -37,27 +74,27 @@ resource "aws_key_pair" "tools" {
   # ~ means windows home directory
 }
 
-module "nexus" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+# module "nexus" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "nexus"
+#   name = "nexus"
 
-  instance_type          = "t3.medium"
-  vpc_security_group_ids = ["sg-0023be3b9dcdb312d"]
-  # convert StringList to list and get first element
-  subnet_id = "subnet-0ea509ad4cba242d7"
-  ami = data.aws_ami.nexus_ami_info.id
-  key_name = aws_key_pair.tools.key_name
-  root_block_device = [
-    {
-      volume_type = "gp3"
-      volume_size = 30
-    }
-  ]
-  tags = {
-    Name = "nexus"
-  }
-}
+#   instance_type          = "t3.medium"
+#   vpc_security_group_ids = ["sg-0023be3b9dcdb312d"]
+#   # convert StringList to list and get first element
+#   subnet_id = "subnet-0ea509ad4cba242d7"
+#   ami = data.aws_ami.nexus_ami_info.id
+#   key_name = aws_key_pair.tools.key_name
+#   root_block_device = [
+#     {
+#       volume_type = "gp3"
+#       volume_size = 30
+#     }
+#   ]
+#   tags = {
+#     Name = "nexus"
+#   }
+# }
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
