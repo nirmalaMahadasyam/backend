483f957459b69985ddc568156e20c1d36e5026b0
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-09-01 15:13:43 +0530

    add nexus in cicd project

--- terraform-aws-tools/cicd/data.tf
+++ terraform-aws-tools/cicd/data.tf
@@ -23,11 +23,14 @@ data "aws_ami" "ami_info" {
 data "aws_ami" "nexus_ami_info" {
 
     most_recent = true
-    owners = ["851725509871"]
+    //owners = ["851725509871"]
+    owners = ["679593333241"]
+
 
     filter {
         name   = "name"
         values = ["SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-*"]
+        # SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-af3afa48-7c5dedd5-58ec-4c82-b20e-fb1f805a0304
     }
 
     filter {
