0aefe05129a5ac15be087834fda3e6df553232b4
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-09-02 13:27:15 +0530

    add nexusRepository in cicd project

--- terraform-aws-tools/cicd/data.tf
+++ terraform-aws-tools/cicd/data.tf
@@ -20,26 +20,26 @@ data "aws_ami" "ami_info" {
     }
 }
 
-data "aws_ami" "nexus_ami_info" {
+# data "aws_ami" "nexus_ami_info" {
 
-    most_recent = true
-    //owners = ["851725509871"]
-    owners = ["679593333241"]
+#     most_recent = true
+#     //owners = ["851725509871"]
+#     owners = ["679593333241"]
 
 
-    filter {
-        name   = "name"
-        values = ["SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-*"]
-        # SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-af3afa48-7c5dedd5-58ec-4c82-b20e-fb1f805a0304
-    }
+#     filter {
+#         name   = "name"
+#         values = ["SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-*"]
+#         # SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-af3afa48-7c5dedd5-58ec-4c82-b20e-fb1f805a0304
+#     }
 
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+#     filter {
+#         name   = "root-device-type"
+#         values = ["ebs"]
+#     }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
+#     filter {
+#         name   = "virtualization-type"
+#         values = ["hvm"]
+#     }
+# }
